c02844567a8a44551d13a27e3e7cf9e2209020ba
ChrisWongAtCUHK <wc38.wong@gmail.com>
2013-12-31 17:36:49 +0800

    An object is considered immutable if its state cannot change after it is constructed.

---
+++ SynchronizedClassExample/src/oracle/concurrency/syncrgb/SynchronizedRGBBadDemo.java
@@ -0,0 +1,41 @@
+package oracle.concurrency.syncrgb;
+
+import static java.lang.System.out;
+
+public class SynchronizedRGBBadDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final SynchronizedRGB color = new SynchronizedRGB(0, 0, 0, "Pitch Black");
+		
+		// If another thread invokes color.set after Statement 1 but before Statement 2, the value of myColorInt won't match the value of myColorName.
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException exception) {
+
+				}
+				int myColorInt = color.getRGB();      // Statement 1
+				String myColorName = color.getName(); // Statement 2
+				
+				out.format("Thread1: myColorInt= %d, myColorName= %s%n", myColorInt, myColorName);
+				
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				color.set(128, 128, 128, "Grey") ;     // Problem may occurs
+				out.format("Thread2: myColorInt= %d, myColorName= %s%n", color.getRGB(), color.getName());
+			}
+		}).start();
+	}
+
+}
---
+++ SynchronizedClassExample/src/oracle/concurrency/syncrgb/SynchronizedRGBProperDemo.java
@@ -0,0 +1,43 @@
+package oracle.concurrency.syncrgb;
+
+import static java.lang.System.out;
+
+public class SynchronizedRGBProperDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		final SynchronizedRGB color = new SynchronizedRGB(0, 0, 0, "Pitch Black");
+		
+		// If another thread invokes color.set after Statement 1 but before Statement 2, the value of myColorInt won't match the value of myColorName.
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(5);
+				} catch (InterruptedException exception) {
+
+				}
+				synchronized (color) {
+				    int myColorInt = color.getRGB();
+				    String myColorName = color.getName();
+				    out.format("Thread1: myColorInt= %d, myColorName= %s%n", myColorInt, myColorName);
+				}
+				
+			}
+		}).start();
+		
+		new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				color.set(128, 128, 128, "Grey") ;     // Problem may occurs
+				out.format("Thread2: myColorInt= %d, myColorName= %s%n", color.getRGB(), color.getName());
+			}
+		}).start();
+
+	}
+
+}
